74e8cddb63f3f3ad2c616560125c7cdd2b3cca74
gununes031 <gununes31@yahoo.com.br>
2025-10-08 22:53:40 -0300

    feat: Implementação completa do Tarefa Manager
    - Implementados testes unitários
    - Implementados testes integrados
    - Configuração de Docker para a aplicação
    - Adicionado README.md detalhado
    - Adicionado Collection do Postman (`TarefaManager.postman_collection.json`)

---
+++ src/test/java/br/com/tarefamanager/TarefaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tarefamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
